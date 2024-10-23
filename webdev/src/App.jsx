242be4a1536ff9f55c297c4b5adcfa6db7db1892
TalhaTR7 <www.talharafat@gmail.com>
2024-10-23 21:26:06 +0500

    Changes Alot

--- webdev/src/App.jsx
+++ webdev/src/App.jsx
@@ -1,7 +1,17 @@
-import Comp1 from './comp1.jsx'
+import {BrowserRouter as Route, Routes, Router} from 'react-router-dom'
+import React from 'react'
+import Services from "./services.jsx"
+
 
 function App() {
-  return (<Comp1 />)
+  return (
+    <Router>
+      <Routes>
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </Router>
+  )
+   
 }
 
 export default App
---
+++ webdev/src/contact.jsx
@@ -0,0 +1,11 @@
+
+function Contact() {
+
+    return (
+        <div>
+            <h2>Contact Us</h2>
+        </div>
+    )
+}
+
+export default Contact

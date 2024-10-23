65c9554169b59fc50dcee61028ac0e2ff7ac4bf4
TalhaTR7 <www.talharafat@gmail.com>
2024-10-23 21:33:07 +0500

    Changes alot again

--- webdev/src/App.jsx
+++ webdev/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Route, Routes, Router} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import React from 'react'
 import Services from "./services.jsx"
 
